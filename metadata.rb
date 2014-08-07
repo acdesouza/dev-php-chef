1f9cddfc8b97fc6673049fa9b8a09b1b86c3170d
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2014-08-07 17:48:29 -0300

    Berksshelf new cookbook.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'dev-php'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures dev-php'
+long_description 'Installs/Configures dev-php'
+version          '0.1.0'
+
